fda134e0c93a1301b5d636c8ea1918e3851873bc
uyudane <sy.yun@icloud.com>
2023-07-06 23:36:23 +0900

    Add: -128p

--- apps/part2/src/main/java/net/bookdevcontainer/todolist/api/repository/SNATTest.java
+++ apps/part2/src/main/java/net/bookdevcontainer/todolist/api/repository/SNATTest.java
@@ -1,44 +1,44 @@
-// package net.bookdevcontainer.todolist.api.repository;
+package net.bookdevcontainer.todolist.api.repository;
 
-// import java.time.ZonedDateTime;
-// import javax.persistence.Entity;
-// import javax.persistence.GeneratedValue;
-// import javax.persistence.GenerationType;
-// import javax.persistence.Id;
+import java.time.ZonedDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
-// @Entity // This tells Hibernate to make a table out of this class
-// public class SNATTest {
-//   @Id
-//   @GeneratedValue(strategy=GenerationType.AUTO)
-//   private Integer id;
+@Entity // This tells Hibernate to make a table out of this class
+public class SNATTest {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
 
-//   private ZonedDateTime createdOn;
+  private ZonedDateTime createdOn;
 
-//   private ZonedDateTime updatedOn;
+  private ZonedDateTime updatedOn;
 
-//   public Integer getId() {
-//     return id;
-//   }
+  public Integer getId() {
+    return id;
+  }
 
-//   public void setId(Integer id) {
-//     this.id = id;
-//   }
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-//   public ZonedDateTime getCreatedOn() {
-//     return createdOn;
-//   }
+  public ZonedDateTime getCreatedOn() {
+    return createdOn;
+  }
 
-//   public void setCreatedOn(ZonedDateTime createdOn) {
-//     this.createdOn = createdOn;
-//   }
+  public void setCreatedOn(ZonedDateTime createdOn) {
+    this.createdOn = createdOn;
+  }
 
-//   public ZonedDateTime getUpdatedOn() {
-//     return updatedOn;
-//   }
+  public ZonedDateTime getUpdatedOn() {
+    return updatedOn;
+  }
 
-//   public void setUpdatedOn(ZonedDateTime updatedOn) {
-//     this.updatedOn = updatedOn;
-//   }
+  public void setUpdatedOn(ZonedDateTime updatedOn) {
+    this.updatedOn = updatedOn;
+  }
 
-// }
+}
